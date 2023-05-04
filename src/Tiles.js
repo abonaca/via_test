c9f8450af0c332c0ccbd5d7e70c7a2e507f923f7
Ana Bonaca <abonaca@gmail.com>
2023-05-03 21:54:31 -0700

    fixed deployment

--- src/Tiles.js
+++ src/Tiles.js
@@ -7,7 +7,7 @@ function Tiles() {
   return (
     <div id="tiles">
       <div className="section">
-        <div className="background" style={{backgroundImage: "url(/img/mw_igor_desktop.jpg)"}}></div>
+        <div className="background" style={{backgroundImage: "url(img/mw_igor_desktop.jpg)"}}></div>
         <div className="inner-left-bottom">
           <h1 style={{textTransform: "uppercase", marginBottom: "10px"}}>The Via Project</h1>
           <h2 style={{textTransform: "uppercase"}}>Into the Milky Way's final frontier</h2>
