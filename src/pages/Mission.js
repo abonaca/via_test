c70bdf84fbbd68c482281ce57f7fec2e1a0cdd8a
Ana Bonaca <abonaca@gmail.com>
2023-05-24 15:16:34 -0700

    mirror mission

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -7,12 +7,12 @@ function App() {
       <Title />
       <div className="section">
         <div className="background" style={{backgroundImage: "url(../img/TNG50_protocluster_med.png)"}}></div>
-        <div className="inner-left-bottom">
+        <div className="inner-right-bottom">
           <h2>Discover</h2>
           <h3 className="indent">The nature of dark matter</h3>
-          <h2 style={{marginTop: "10px"}}>Find</h2>
+          <h2 style={{marginTop: "25px"}}>Find</h2>
           <h3 className="indent">The edge of galaxy formation</h3>
-          <h2 style={{marginTop: "10px"}}>Map</h2>
+          <h2 style={{marginTop: "25px"}}>Map</h2>
           <h3 className="indent">The cold gas feeding the Milky Way</h3>
         </div>
       </div>
