dab1a5a23640807e703b8d3b65523c2a0a8317e2
Ana Bonaca <abonaca@gmail.com>
2023-05-27 18:52:11 -0700

    bench spectrograph section

--- src/pages/Spectrographs.js
+++ src/pages/Spectrographs.js
@@ -19,7 +19,7 @@ function App() {
         <div className="inner-left-bottom" style={{width:"30%", left:"100px"}}>
           <h4 style={{textTransform:"uppercase", paddingBottom:"5px"}}>Overview</h4>
           <h3 style={{paddingBottom:"5px"}}>System Design</h3>
-          <p>Viaspec will be mounted at the f/5 Cassegrain focus. The fiber positioner system will be attached to the primary mirror with six spokes. The fiber run will be kept short to minimize the throughput loss. The bench spectrograph will be positioned on the telescope platform, and store on a cart for easier storage.</p>
+          <p>Viaspec will be mounted at the f/5 Cassegrain focus. The fiber positioner system will be attached to the primary mirror cell with six struts. The fiber run will be kept short to minimize the throughput loss. The bench spectrograph will be positioned on the telescope platform, and store on a cart for easier storage.</p>
         </div>
       </div>
       
@@ -32,10 +32,35 @@ function App() {
         <table className="data">
             <tbody>
               <tr>
-                <td>Fiber Size</td><td>1.15"<span> / 0.19mm</span></td>
+                <td>Fiber size</td><td>1.15"<span> / 0.2mm</span></td>
               </tr>
               <tr>
                 <td>Patrol region</td><td>155"<span> / 25.9mm</span></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      
+      <div className="section">
+        <div className="background" style={{backgroundImage: "url(../img/viaspec_optics_med.png)"}}></div>
+        <div className="inner-left-bottom" style={{width:"30%", left:"100px"}}>
+          <h4 style={{textTransform:"uppercase", paddingBottom:"5px"}}>Component</h4>
+          <h3 style={{paddingBottom:"5px"}}>Bench Spectrograph</h3>
+          <p>Viaspec will have a 260 mm beam spectrograph to accommodate 600 fibers. The camera is a simplified version of the GMACS blue channel design with very high image quality of ~25µm RMS diameter. The optical reduction factor is 2.29, so a 1.15" fiber forms an ~90µm diameter image. The spherical collimator mirror is ~800mm in diameter. The spectrograph is to be located in a gravity-invariant, thermally stable enclosure.</p>
+        <table className="data">
+            <tbody>
+              <tr>
+                <td>Resolution</td><td>15,000</td>
+              </tr>
+              <tr>
+                <td>Binary grating</td><td>2,600 lpm</td>
+              </tr>
+              <tr>
+                <td>Spectral coverage</td><td>{'510 nm \u2014 595 nm'}</td>
+              </tr>
+              <tr>
+                <td>CCD format</td><td>9K x 9K</td>
               </tr>
             </tbody>
           </table>
