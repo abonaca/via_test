5f6d41b350ba9e4aec8ff3978ec3dd8314ae1fde
Ana Bonaca <abonaca@gmail.com>
2023-05-27 23:20:16 -0700

    tweak viaspec figures and text

--- src/pages/Spectrographs.js
+++ src/pages/Spectrographs.js
@@ -19,7 +19,8 @@ function App() {
         <div className="inner-left-bottom" style={{width:"30%", left:"100px"}}>
           <h4 style={{textTransform:"uppercase", paddingBottom:"5px"}}>Overview</h4>
           <h3 style={{paddingBottom:"5px"}}>System Design</h3>
-          <p>Viaspec will be mounted at the f/5 Cassegrain focus. The fiber positioner system will be attached to the primary mirror cell with six struts. The fiber run will be kept short to minimize the throughput loss. The bench spectrograph will be positioned on the telescope platform, and store on a cart for easier storage.</p>
+          <p>ViaSpec has three main systems: (1) the fiber positioner, mounted at the f/5 Cassegrain focus and attached to the primary mirror cell with six struts, (2) a ~25m run of optical fibers from the fiber positioner to the spectrograph, kept short to minimize throughput losses, and (3) the bench spectrograph, mounted on a cart for convenient storage, and parked directly on the telescope platform.</p>
+          <p>In addition, ViaSpec will have a camera-based metrology system to provide iterative fiber positioning feedback, guiders and wavefront sensors, and a set of fixed fibers that can be sent to additional spectrographs, possibly serving exoplanet or transient research.</p>
         </div>
       </div>
       
@@ -28,7 +29,7 @@ function App() {
         <div className="inner-left-bottom" style={{width:"30%", left:"100px"}}>
           <h4 style={{textTransform:"uppercase", paddingBottom:"5px"}}>Component</h4>
           <h3 style={{paddingBottom:"5px"}}>Focal Plane System</h3>
-          <p>ViaSpec will be fed by 600 robotic fibers, positioned in the focal plane by the vertically-orientetd theta/phi actuators, developed by MPS. The fibers can be positioned nearly simultaneously and the system is very fault tolerant. The optical fibers will travel on the neutral axis of a controlled-radius cable carrier in the fiber derotator and between the fiber positioner and the bench spectrograph.</p>
+          <p>ViaSpec will be fed by 600 robotic fibers, positioned in the focal plane by the vertically-oriented theta/phi actuators (developed by <a href="https://www.mpsag.com/en/">MPS</a>). This configuration allows for nearly simultaneously positioning of all fibers, and features high fault tolerance. Optical fibers will travel on the neutral axis of a controlled-radius cable carrier in the fiber derotator, and between the fiber positioner and the bench spectrograph.</p>
         <table className="data">
             <tbody>
               <tr>
@@ -47,7 +48,7 @@ function App() {
         <div className="inner-left-bottom" style={{width:"30%", left:"100px"}}>
           <h4 style={{textTransform:"uppercase", paddingBottom:"5px"}}>Component</h4>
           <h3 style={{paddingBottom:"5px"}}>Bench Spectrograph</h3>
-          <p>Viaspec will have a 260 mm beam spectrograph to accommodate 600 fibers. The camera is a simplified version of the GMACS blue channel design with very high image quality of ~25µm RMS diameter. The optical reduction factor is 2.29, so a 1.15" fiber forms an ~90µm diameter image. The spherical collimator mirror is ~800mm in diameter. The spectrograph is to be located in a gravity-invariant, thermally stable enclosure.</p>
+          <p>Viaspec will have a 260 mm beam spectrograph to accommodate 600 fibers, which will be located in a gravity-invariant, thermally stable enclosure. It will employ a binary grating that spans the spectral region between the Mgb triplet (for precise radial velocity measurement) and the NaD doublet (for detecting interstellar gas) with a uniformly high efficiency of ~90%. The camera has six optical elements and produces a high-quality image on a single large-frame CCD.</p>
         <table className="data">
             <tbody>
               <tr>
@@ -60,7 +61,7 @@ function App() {
                 <td>Spectral coverage</td><td>{'510 nm \u2014 595 nm'}</td>
               </tr>
               <tr>
-                <td>CCD format</td><td>9K x 9K</td>
+                <td>CCD format</td><td>10K x 10K</td>
               </tr>
             </tbody>
           </table>
