5af0af6d60bd38b6fb181d2e4e277c4e30e1f4e9
Ana Bonaca <abonaca@gmail.com>
2023-05-29 15:20:55 -0700

    mission text tweaks

--- src/pages/Mission.js
+++ src/pages/Mission.js
@@ -12,7 +12,7 @@ function App() {
           What are the limits of galaxy formation?<br/>
           How does gas cycle into and out of galaxies?</p>
           
-          <p>The answer to these questions lies in the high-resolution positions and velocities of stars and gas in the Milky Way halo, beyond the precision and scope of current instruments. </p>
+          <p>The answer to these questions lies in the high-resolution map and motions of stars and gas in the Milky Way, beyond the precision and scale of current astronomical instrumentation.</p>
         </div>
       </div>
       
@@ -23,8 +23,8 @@ function App() {
         <div className="inner-left-bottom" style={{width:"30%", left:"100px"}}>
           <h4 style={{textTransform:"uppercase", paddingBottom:"5px"}}>Overview</h4>
           <h3 style={{paddingBottom:"5px"}}>The Via Project</h3>
-          <p>The Via Project is using the Milky Way galaxy as a laboratory to answer fundamental questions about the nature of the universe. Via will conduct an all-sky survey of stars using the 6.5-meter MMT (Arizona) and Magellan (Chile) telescopes. The survey will utilize the ViaSpec instrument, which will be built and deployed on both telescopes.</p>
-          <p>The project is a collaboration between Carnegie Observatories, the Center for Astrophysics | Harvard & Smithsonian, and the University of Arizona and includes experts in conducting large-scale surveys and building astronomical instrumentation. We expect to begin the survey in early 2027 in order to capitalize on the enabling data provided by Gaia DR4 and LSST.</p>
+          <p>The Via Project is using the Milky Way galaxy as a laboratory to answer fundamental questions about the nature of the universe. Via will conduct an all-sky survey of stars using the 6.5-meter MMT (Arizona) and Magellan (Chile) telescopes. The survey will utilize the ViaSpec instruments, which will be built and deployed on each of the telescopes.</p>
+          <p>The project is a collaboration between Carnegie Observatories, the Center for Astrophysics | Harvard & Smithsonian, and the University of Arizona. Our team includes experts in conducting large-scale surveys and building astronomical instrumentation. We expect to begin the survey in early 2027 to immediately build on the enabling data released by Gaia DR4 and LSST.</p>
         </div>
       </div>
       
